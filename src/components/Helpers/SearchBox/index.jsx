56ca5c0c53076789ed95f4a5d96d010c66e924e1
Ricardo Roque <130297657+ricaDeveloper@users.noreply.github.com>
2023-10-29 17:26:53 -0300

    translation searchbox+correção dropzone

--- src/components/Helpers/SearchBox/index.jsx
+++ src/components/Helpers/SearchBox/index.jsx
@@ -55,7 +55,7 @@ export default function SearchBox({ className, type }) {
           }
           type="button"
         >
-          Search
+          Pesquisar
         </button>
       </div>
     </>
