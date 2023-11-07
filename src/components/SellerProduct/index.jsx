172417bd8259c0e41aacf7e59d0686efec7cb866
Henri costa rios <119058648+henricr109@users.noreply.github.com>
2023-11-07 13:20:02 -0300

    Update index.jsx
    imagem adicionar produto

--- src/components/SellerProduct/index.jsx
+++ src/components/SellerProduct/index.jsx
@@ -1,7 +1,6 @@
 import { useRef, useState } from "react";
 import Swal from "sweetalert2";
 import api from "../../services/api";
-import Dropzone from "../Helpers/Dropzone";
 import InputCom from "../Helpers/InputCom";
 import InputTextareaCom from "../Helpers/InputTextareaCom";
 import PageTitle from "../Helpers/PageTitle";
@@ -65,6 +64,7 @@ export default function SellerProduct() {
       value: productPrice,
       race: productSubCategory,
       description: productDescription,
+      productImage: profileImg,
       fkUserId: id
       //Falta adicionar a imagem do anúncio
     };
@@ -275,20 +275,46 @@ export default function SellerProduct() {
                       Preencha todos os campos do anúncio.
                     </p>
                   </div>
-                  <div className="update-cover w-full">
-                    <h1 className="text-sm tracking-wide text-qblack flex items-center mb-2">
-                      Por favor insira as imagens do anúncio (limite de 5
-                      imagens)
-                    </h1>
-                    <Dropzone
-                      limitFiles={10}
-                      className={
-                        "input-item h-[100px] border border-[#EDEDED] text-sm text-qgraytwo px-5  text-center flex items-center justify-center mb-5"
-                      }
-                      acceptType={"image/*"}
-                      filesInserted={(files) => console.log(files)}
-                    />
+                  <div className="flex justify-center">
+                    <div className="w-full relative">
+                      <img
+                        src={
+                          profileImg ||
+                          `${process.env.PUBLIC_URL}/assets/images/edit-coverimg.jpg`
+                        }
+                        alt=""
+                        className="w-full h-[120px] rounded-lg overflow-hidden object-cover"
+                      />
+                      <input
+                        ref={profileImgInput}
+                        onChange={(e) => profileImgChangHandler(e)}
+                        type="file"
+                        className="hidden"
+                      />
+                      <div
+                        onClick={browseProfileImg}
+                        className="w-[32px] h-[32px] absolute -bottom-4 right-4 bg-[#87D6D1] hover:bg-[#87D6D1] rounded-full cursor-pointer"
+                      >
+                        <svg
+                          width="32"
+                          height="32"
+                          viewBox="0 0 32 32"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M16.5147 11.5C17.7284 12.7137 18.9234 13.9087 20.1296 15.115C19.9798 15.2611 19.8187 15.4109 19.6651 15.5683C17.4699 17.7635 15.271 19.9587 13.0758 22.1539C12.9334 22.2962 12.7948 22.4386 12.6524 22.5735C12.6187 22.6034 12.5663 22.6296 12.5213 22.6296C11.3788 22.6334 10.2362 22.6297 9.09365 22.6334C9.01498 22.6334 9 22.6034 9 22.536C9 21.4009 9 20.2621 9.00375 19.1271C9.00375 19.0746 9.02997 19.0109 9.06368 18.9772C10.4123 17.6249 11.7609 16.2763 13.1095 14.9277C14.2295 13.8076 15.3459 12.6913 16.466 11.5712C16.4884 11.5487 16.4997 11.5187 16.5147 11.5Z"
+                            fill="white"
+                          />
+                          <path
+                            d="M20.9499 14.2904C19.7436 13.0842 18.5449 11.8854 17.3499 10.6904C17.5634 10.4694 17.7844 10.2446 18.0054 10.0199C18.2639 9.76139 18.5261 9.50291 18.7884 9.24443C19.118 8.91852 19.5713 8.91852 19.8972 9.24443C20.7251 10.0611 21.5492 10.8815 22.3771 11.6981C22.6993 12.0165 22.7105 12.4698 22.3996 12.792C21.9238 13.2865 21.4443 13.7772 20.9686 14.2717C20.9648 14.2792 20.9536 14.2867 20.9499 14.2904Z"
+                            fill="white"
+                          />
+                        </svg>
+                      </div>
+                    </div>
                   </div>
+
                   <div className="input-area pt-2">
                     <div className="flex sm:flex-row flex-col space-y-5 sm:space-y-0 sm:space-x-5 mb-5">
                       <InputCom
