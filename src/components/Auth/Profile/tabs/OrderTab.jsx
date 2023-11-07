2155964e374c4c18873c5474b78eb613af02e582
Henri costa rios <119058648+henricr109@users.noreply.github.com>
2023-11-07 01:05:18 -0300

    Update OrderTab.jsx
    Update profile pedidos feitos
    não sei usar useState direito então todos estao com o mesmo valor

--- src/components/Auth/Profile/tabs/OrderTab.jsx
+++ src/components/Auth/Profile/tabs/OrderTab.jsx
@@ -1,6 +1,25 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import api from "../../../../services/api";
 export default function OrderTab() {
+  const [date, setDate] = useState("");
+  const [situation, setSituation] = useState("");
+  const [value, setValue] = useState("");
+
+  const orderHandler = async () => {
+    let id = JSON.parse(localStorage.getItem("user"));
+    id = id.id;
+    console.log(id)
+    const response = await api.get(`/orders/userId/${id}`);
+    const { data } = response;
+    console.log(data);
+    var array = data
+    var obj = array[0]
+    setDate(obj.date);
+    setSituation(obj.situation);
+    setValue(obj.value);
+  }
+  useEffect(() => { orderHandler() }, [])
+
   return (
     <>
       <div className="relative w-full overflow-x-auto sm:rounded-lg">
@@ -13,204 +32,107 @@ export default function OrderTab() {
               </td>
               <td className="py-4 whitespace-nowrap text-center">Data</td>
               <td className="py-4 whitespace-nowrap text-center">Status</td>
-              <td className="py-4 whitespace-nowrap text-center">Estoque</td>
-              <td className="py-4 whitespace-nowrap  text-center">Ação</td>
+              <td className="py-4 whitespace-nowrap text-center">Preço</td>
             </tr>
             {/* table heading end */}
             <tr className="bg-white border-b hover:bg-gray-50">
               <td className="text-center py-4">
-                <span className="text-lg text-qgray font-medium">#354</span>
+                <span className="text-lg text-qgray font-medium">#1</span>
               </td>
               <td className="text-center py-4 px-2">
                 <span className="text-base text-qgray  whitespace-nowrap">
-                  Fev 05,2021
+                  {date}
                 </span>
               </td>
               <td className="text-center py-4 px-2">
                 <span className="text-sm rounded text-green-500 bg-green-100 p-2">
-                  Concluído
+                  {situation}
                 </span>
               </td>
               <td className="text-center py-4 px-2">
                 <span className="text-base text-qblack whitespace-nowrap px-2 ">
-                  RR$757
+                  R${value}
                 </span>
-              </td>
-              <td className="text-center py-4">
-                <button
-                  type="button"
-                  className="w-[116px] h-[46px] bg-qh2-green text-white font-bold"
-                >
-                  Ver Detalhes
-                </button>
               </td>
             </tr>
             <tr className="bg-white border-b hover:bg-gray-50">
               <td className="text-center py-4">
-                <span className="text-lg text-qgray font-medium">#354</span>
+                <span className="text-lg text-qgray font-medium">#2</span>
               </td>
               <td className="text-center py-4 px-2">
                 <span className="text-base text-qgray  whitespace-nowrap">
-                  Fev 05,2021
+                  {date}
                 </span>
               </td>
               <td className="text-center py-4 px-2">
-                <span className="text-sm rounded text-green-500 bg-green-100 p-2">
-                  Concluído
+                <span className="text-sm rounded text-red-500 bg-red-100 p-2">
+                  {situation}
                 </span>
               </td>
               <td className="text-center py-4 px-2">
                 <span className="text-base text-qblack whitespace-nowrap px-2 ">
-                  R$757
+                  R${value}
                 </span>
-              </td>
-              <td className="text-center py-4">
-                <button
-                  type="button"
-                  className="w-[116px] h-[46px] bg-qh2-green text-white font-bold"
-                >
-                  Ver Detalhes
-                </button>
               </td>
             </tr>
             <tr className="bg-white border-b hover:bg-gray-50">
               <td className="text-center py-4">
-                <span className="text-lg text-qgray font-medium">#354</span>
+                <span className="text-lg text-qgray font-medium">#3</span>
               </td>
               <td className="text-center py-4 px-2">
                 <span className="text-base text-qgray  whitespace-nowrap">
-                  Fev 05,2021
+                  {date}
                 </span>
               </td>
               <td className="text-center py-4 px-2">
-                <span className="text-sm rounded text-green-500 bg-green-100 p-2">
-                  Concluído
+                <span className="text-sm rounded text-red-500 bg-red-100 p-2">
+                  {situation}
                 </span>
               </td>
               <td className="text-center py-4 px-2">
                 <span className="text-base text-qblack whitespace-nowrap px-2 ">
-                  R$757
+                  R${value}
                 </span>
-              </td>
-              <td className="text-center py-4">
-                <button
-                  type="button"
-                  className="w-[116px] h-[46px] bg-qh2-green text-white font-bold"
-                >
-                  Ver Detalhes
-                </button>
               </td>
             </tr>
             <tr className="bg-white border-b hover:bg-gray-50">
               <td className="text-center py-4">
-                <span className="text-lg text-qgray font-medium">#354</span>
+                <span className="text-lg text-qgray font-medium">#4</span>
               </td>
               <td className="text-center py-4 px-2">
                 <span className="text-base text-qgray  whitespace-nowrap">
-                  Fev 05,2021
+                  {date}
                 </span>
               </td>
               <td className="text-center py-4 px-2">
                 <span className="text-sm rounded text-green-500 bg-green-100 p-2">
-                  Concluído
+                  {situation}
                 </span>
               </td>
               <td className="text-center py-4 px-2">
                 <span className="text-base text-qblack whitespace-nowrap px-2 ">
-                  R$757
+                  R${value}
                 </span>
-              </td>
-              <td className="text-center py-4">
-                <button
-                  type="button"
-                  className="w-[116px] h-[46px] bg-qh2-green text-white font-bold"
-                >
-                  Ver Detalhes
-                </button>
               </td>
             </tr>
             <tr className="bg-white border-b hover:bg-gray-50">
               <td className="text-center py-4">
-                <span className="text-lg text-qgray font-medium">#354</span>
+                <span className="text-lg text-qgray font-medium">#5</span>
               </td>
               <td className="text-center py-4 px-2">
                 <span className="text-base text-qgray  whitespace-nowrap">
-                  Fev 05,2021
+                  {date}
                 </span>
               </td>
               <td className="text-center py-4 px-2">
-                <span className="text-sm rounded text-green-500 bg-green-100 p-2">
-                  Concluído
+                <span className="text-sm rounded text-red-500 bg-red-100 p-2">
+                  {situation}
                 </span>
               </td>
               <td className="text-center py-4 px-2">
                 <span className="text-base text-qblack whitespace-nowrap px-2 ">
-                  R$757
+                  R${value}
                 </span>
-              </td>
-              <td className="text-center py-4">
-                <button
-                  type="button"
-                  className="w-[116px] h-[46px] bg-qh2-green text-white font-bold"
-                >
-                  Ver Detalhes
-                </button>
-              </td>
-            </tr>
-            <tr className="bg-white border-b hover:bg-gray-50">
-              <td className="text-center py-4">
-                <span className="text-lg text-qgray font-medium">#354</span>
-              </td>
-              <td className="text-center py-4 px-2">
-                <span className="text-base text-qgray  whitespace-nowrap">
-                  Fev 05,2021
-                </span>
-              </td>
-              <td className="text-center py-4 px-2">
-                <span className="text-sm rounded text-green-500 bg-green-100 p-2">
-                  Concluído
-                </span>
-              </td>
-              <td className="text-center py-4 px-2">
-                <span className="text-base text-qblack whitespace-nowrap px-2 ">
-                  R$757
-                </span>
-              </td>
-              <td className="text-center py-4">
-                <button
-                  type="button"
-                  className="w-[116px] h-[46px] bg-qh2-green text-white font-bold"
-                >
-                  Ver Detalhes
-                </button>
-              </td>
-            </tr>
-            <tr className="bg-white border-b hover:bg-gray-50">
-              <td className="text-center py-4">
-                <span className="text-lg text-qgray font-medium">#354</span>
-              </td>
-              <td className="text-center py-4 px-2">
-                <span className="text-base text-qgray  whitespace-nowrap">
-                  Fev 05,2021
-                </span>
-              </td>
-              <td className="text-center py-4 px-2">
-                <span className="text-sm rounded text-green-500 bg-green-100 p-2">
-                  Concluído
-                </span>
-              </td>
-              <td className="text-center py-4 px-2">
-                <span className="text-base text-qblack whitespace-nowrap px-2 ">
-                  R$757
-                </span>
-              </td>
-              <td className="text-center py-4">
-                <button
-                  type="button"
-                  className="w-[116px] h-[46px] bg-qh2-green text-white font-bold"
-                >
-                  Ver Detalhes
-                </button>
               </td>
             </tr>
           </tbody>
