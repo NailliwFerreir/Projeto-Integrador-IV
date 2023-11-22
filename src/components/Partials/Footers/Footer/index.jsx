7f17e01c20656e5e6aca5793990cf9726c9290b7
Henri costa rios <henricostarios@gmail.com>
2023-11-22 08:24:14 -0300

    uptade footer
    Co-Authored-By: Paulo Vítor Reis <129749259+paulovreis@users.noreply.github.com>

--- src/components/Partials/Footers/Footer/index.jsx
+++ src/components/Partials/Footers/Footer/index.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import Facebook from "../../../Helpers/icons/Facebook";
 import Instagram from "../../../Helpers/icons/Instagram";
 import Youtube from "../../../Helpers/icons/Youtube";
@@ -7,48 +6,6 @@ export default function Footer() {
   return (
     <footer className="footer-section-wrapper bg-white">
       <div className="container-x block mx-auto pt-[83px]">
-        <div className="lg:flex justify-between mb-[95px]">
-          <div className="lg:w-4/10 w-full mb-10 lg:mb-0">
-            {/* logo area */}
-            <div className="mb-14">
-              <a href="/">
-                <img
-                  width="152"
-                  height="36"
-                  src={`${process.env.PUBLIC_URL}/assets/images/INSEMINA +.svg`}
-                  alt="logo"
-                />
-              </a>
-            </div>
-            <div>
-              <ul className="flex flex-col space-y-5 ">
-                <li>
-                  <Link to="/">
-                    <span className="text-[#9A9A9A] text-[15px] hover:text-qblack border-b border-transparent hover:border-qblack">
-                      Rastrear Pedido
-                    </span>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/">
-                    <span className="text-[#9A9A9A] text-[15px] hover:text-qblack border-b border-transparent hover:border-qblack">
-                      Entregas & Devoluções
-                    </span>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/">
-                    <span className="text-[#9A9A9A] text-[15px] hover:text-qblack border-b border-transparent hover:border-qblack">
-                      Garantia
-                    </span>
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="lg:w-2/10 w-full mb-10 lg:mb-0">
-          </div>
-        </div>
         <div className="bottom-bar border-t border-qgray-border lg:h-[82px] lg:flex justify-between items-center">
           <div className="flex lg:space-x-5 justify-between items-center mb-3">
             <div className="flex space-x-5 items-center">
